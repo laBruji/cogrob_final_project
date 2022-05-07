6731e51a924dfbbfc3cb8273f52a13c94c6365cf
laBruji <htromero66@gmail.com>
2022-05-07 12:03:02 -0400

    adding online dispatcher

---
+++ dispatcher.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Dispatcher class to help with online scheduling
+"""
+
+import time
+import numpy as np
+
+class Dispatcher():
+    def __init__(self, disturbance_max_delay=0.0, sim_time=False, quiet=False):
+        self.disturbance_max_delay = disturbance_max_delay
+        self.sim_time = sim_time
+        self.quiet = quiet
+
+        self.time_start = None
+        self.t = None
+        self.noisy_events = set(['C', 'E'])
+        self.execution_trace = {}
+
+    def start(self):
+        if self.sim_time:
+            self.time_start = 0.0
+            self.t = 0.0
+        else:
+            self.time_start = time.time()
+
+        if not self.quiet:
+            self.print_time_message("Starting", 0.0)
+        return self.time_start
+
+    def done(self):
+        if not self.quiet:
+            self.print_time_message("Done!", self.time())
+
+    def time(self):
+        if self.sim_time:
+            return self.t
+        else:
+            return time.time() - self.time_start
+
+    def sleep(self, amount):
+        if amount < 0:
+            raise Exception("Called sleep for a negative amount of time!")
+
+        if self.sim_time:
+            self.t += amount
+        else:
+            time.sleep(amount)
+
+    def dispatch(self, event):
+        if self.time_start is None:
+            raise Exception("You need to call dispatcher.start() before you dispatch anything.")
+        if event in self.execution_trace:
+            raise Exception("Trying to dispatch event {}, but it was already dispatched earlier!".format(event))
+
+        if event in self.noisy_events:
+            delay = self.simulate_disturbance()
+        else:
+            delay = 0.0
+        t = self.time()
+        self.execution_trace[event] = t
+
+        if not self.quiet:
+            self.print_time_message("Dispatched {} {}".format(
+                                        event, ("" if delay == 0.0 else "(delay {:0.4f}s)".format(delay))),
+                                    t)
+        return t
+    
+    def simulate_disturbance(self):
+        delay = np.random.random() * self.disturbance_max_delay
+        self.sleep(delay)
+        return delay
+
+    def print_time_message(self, message, time):
+        print("\033[33m{:08.4f}\033[0m: {}".format(time, message))
+
--- mastnu_to_dispatch.py
+++ mastnu_to_dispatch.py
@@ -1,5 +1,6 @@
 import networkx as nx
 import numpy as np
+from dispatcher import Dispatcher
 
 from temporal_network import (SimpleContingentTemporalConstraint,
                                           SimpleTemporalConstraint,
@@ -53,8 +54,81 @@ def get_independent_STNUs(decoupling, agent):
     return d_graph
 
 def dc_checking(d_graph):
+    # TODO
     pass
 
+def online_dispatch(dispatchable_form, dispatcher):
+    """Algorithm to dispatch online. It should use the Python time module and make
+    sure that events are executed as close as possible to the proper times. Method
+    should not return until enough real-world time has passed to execute the entire plan.
+    """
+    N = dispatchable_form.number_of_nodes()
+    events = list(dispatchable_form.nodes())
+    unscheduled_events = set(dispatchable_form.nodes())
+    exec_windows = {k:(-np.inf, np.inf) for k in events}
+    enabled_events = []
+    executed_events = set()
+    
+    def propagate(event, t_i):
+        in_edges = list(dispatchable_form.in_edges(event, data=True))
+        out_edges = list(dispatchable_form.out_edges(event, data=True))
+        for i in out_edges:
+            # get associated event
+            e = i[1]
+            if e not in executed_events:
+                new_up = min(exec_windows[e][1], t_i + i[2]["weight"])
+                exec_windows[e] = (exec_windows[e][0], new_up)
+
+        for i in in_edges:
+            # get associated event
+            e = i[0]
+            if e not in executed_events:
+                new_low = max(exec_windows[e][0], t_i - i[2]["weight"])
+                exec_windows[e] = (new_low, exec_windows[e][1])
+            
+    def get_predecessors(event):
+        out_edges = list(dispatchable_form.out_edges(event, data=True))
+        pred = set()
+        for i in out_edges:
+            if i[2]["weight"] < 0:
+                pred.add(i[1])
+        return pred
+    
+    predecessors = {}
+    for e in events:
+        predecessors[e] = get_predecessors(e)
+        if len(predecessors[e]) == 0:
+            enabled_events.append(e)
+    
+    # Start dispatching!
+    dispatcher.start()
+    
+    while len(executed_events) != N:
+        # sort current events
+        enabled_events = sorted(enabled_events, key=lambda x: x[0])
+        # get event that should happen first
+        current_event = enabled_events[0]
+        # wait until it's time to dispatch it
+        to_wait = exec_windows[current_event][0] - dispatcher.time()
+        if to_wait > 0:
+            dispatcher.sleep(to_wait)
+        
+        if current_event not in executed_events:
+            t_i = dispatcher.dispatch(current_event)
+            exec_windows[current_event] = (t_i, t_i)
+            propagate(current_event, t_i)
+            executed_events.add(current_event)
+            enabled_events.remove(current_event)
+
+            # update enabled events 
+            for e in events:
+                if e not in executed_events and e not in enabled_events:
+                    if predecessors[e].issubset(executed_events):
+                        enabled_events.append(e)
+
+        
+    # All done!
+    dispatcher.done()
 
 def main():
     mastnu, agents = example_stnu()
@@ -67,6 +141,9 @@ def main():
 
         # dc-checking algorithm
         dispatchable = dc_checking(d_graph)
+
+        dispatcher = Dispatcher()
+        online_dispatch(dispatchable, dispatcher)
 
 if __name__ == "__main__":
     main()
